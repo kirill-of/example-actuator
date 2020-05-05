6197e2d5ecc257faa0d2593dfa0866758eec3e16
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2020-05-05 18:05:06 +0400

    Initial commit

---
+++ src/main/java/ru/ofitserov/example/actuator/ExampleActuatorApplication.java
@@ -0,0 +1,13 @@
+package ru.ofitserov.example.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ofitserov/example/actuator/ExampleActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ofitserov.example.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
